db7ce3e182905c2c4ca5f0a922caac556a19b08a
Cristian D Biancotti <cdbiancotti@gmail.com>
2025-07-30 22:36:44 -0300

    Se creo la primer vista en la app

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%1rn#6_98#e5w#5!_k&it3m_*s-=74k9x(u0a7*0oe+2h9$j^='
+# SECRET_KEY = 'django-insecure-%1rn#6_98#e5w#5!_k&it3m_*s-=74k9x(u0a7*0oe+2h9$j^='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inicio'
 ]
 
 MIDDLEWARE = [
